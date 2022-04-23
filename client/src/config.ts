13f136b0e2f13a0326c3a9f4a6c30809cd989164
gbengaoyetade <ioyetade@gmail.com>
2022-04-23 20:30:04 +0100

    update config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = process.env.REACT_APP_API_ID
+const apiId = 'xmfoqeejv9'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: process.env.REACT_APP_DOMAIN, // Auth0 domain
-  clientId: process.env.REACT_APP_CLIENT_ID, // Auth0 client id
+  domain: 'dev-mo9bzlrz.us.auth0.com', // Auth0 domain
+  clientId: 'SKd0pEW2wxxq6Lh2v6aG6aHilk98PQt8', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
